45691069b191edd7aba588f6e817b56e059e1018
Rene Sandoval <sandovalrenemc@gmail.com>
2025-03-08 20:51:48 -0500

    Min 37:47

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
 import { Navbar } from './Components/Navbar/Navbar';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Shop } from './Pages/Shop';
+import { ShopCategory } from './Pages/ShopCategory';
+import { Product } from './Pages/Product';
+import { Cart } from './Pages/Cart';
+import { LoginSignup } from './Pages/LoginSignup';
 function App() {
   return (
     <div>
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop />} />
+          <Route path='/men' element={<ShopCategory category="mena" />} />
+          <Route path='/woman' element={<ShopCategory category="woman" />} />
+          <Route path='/kid' element={<ShopCategory category="kid" />} />
+          <Route path='/product' element={<Product />}>
+            <Route path=':productId' element={<Product />} />
+          </Route>
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<LoginSignup />} />
+
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ frontend/src/Components/Hero/Hero.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Hero.css'
+import hand_icon from '../Assets/hand_icon.png'
+import arrow_icon from '../Assets/arrow.png'
+import hero_image from '../Assets/hero_image.png'
+export const Hero = () => {
+    return (
+        <div className='hero'>
+            <div className="hero-left">
+                <h2>New Arrivals</h2>
+                <div>
+                    <div className="hand-hand-icon">
+                        <p>new</p>
+                        <img src={hand_icon} alt="" />
+                    </div>
+                    <p>collectios</p>
+                    <p>from everyone</p>
+                </div>
+                <div className='hero-lastest-btn'>
+                    <div>latest collection</div>
+                    <img src={arrow_icon} alt="" />
+
+                </div>
+
+            </div>
+            <div className="hero-right">
+                <img src={hero_image} alt="" />
+
+            </div>
+        </div>
+    )
+}
