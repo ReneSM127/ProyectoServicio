029bb716a2a378e59facb5e2b092dde1c4b1361e
Isabel Tec <l21530368@cancun.tecnm.mx>
2025-03-14 21:48:43 -0500

    me quede min 1:05

---
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -0,0 +1,9 @@
+import react from "react"
+import './NewCollections'
+import exclusive_image from '.. /Assets/exclusive_image.png'
+const Offers =() => {
+    return ()
+
+}
+
+export default <NewCollections></NewCollections>
