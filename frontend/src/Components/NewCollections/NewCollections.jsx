b884e4f7f3692ee54fe0f3e8098ec29ba11d4236
Rene Sandoval <sandovalrenemc@gmail.com>
2025-03-16 12:55:11 -0500

    Corrigiendo errores (antes de newspapper)

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -1,9 +1,21 @@
-import react from "react"
-import './NewCollections'
-import exclusive_image from '.. /Assets/exclusive_image.png'
-const Offers =() => {
-    return ()
+import React from 'react'
+import './NewCollections.css'
+import new_collections from '../Assets/new_collections'
+import Item from '../Item/Item'
 
+const NewCollections = () => {
+    return (
+        <div className='new-collections'>
+            <h1>NEW COLLECTIONS</h1>
+            <hr />
+            <div className="collections">
+                {new_collections.map((item, i)=>{
+                    return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+
+                })}
+            </div>
+        </div>
+    )
 }
 
-export default <NewCollections></NewCollections>
+export default NewCollections
--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Hero } from '../Components/Hero/Hero'
+import {Hero} from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
 import Offers from '../Components/Offers/Offers'
 import NewCollections from '../Components/NewCollections/NewCollections'
 
-const Shop = () => {
+export const Shop = () => {
   return (
     <div>
         <Hero/>
@@ -15,4 +15,4 @@ const Shop = () => {
   )
 }
 
-export default Shop
+//export default Shop
