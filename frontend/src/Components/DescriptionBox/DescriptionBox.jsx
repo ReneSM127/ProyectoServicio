15a96f455117b4ef54286f3ec27178566275e1c5
Rene Sandoval <sandovalrenemc@gmail.com>
2025-03-19 18:40:33 -0500

    Min 2:22:14

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-descriptionbox">
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus quos at provident fugiat odit consectetur vitae nihil amet, nesciunt aperiam quo. Maxime eaque dolores reprehenderit eligendi labore dolorum et magnam?
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi nihil consectetur asperiores deserunt natus eius, ab velit fugit officiis harum reiciendis ducimus obcaecati laborum ut optio dolores architecto commodi cupiditate!
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
