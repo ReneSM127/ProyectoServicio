f1ed3774e9cbe1b81528b050d1ae1afdb4b2e416
Rene Sandoval <sandovalrenemc@gmail.com>
2025-04-07 19:49:21 -0500

    Haciendo responsive

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -7,11 +7,11 @@ const Item = (props) => {
             <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0,0)} src={props.image} alt="" /></Link>
             <p>{props.name}</p>
             <div className="item-prices">
-                <div className="item-prices-new">
+                <div className="item-price-new">
                     ${props.new_price}
                 </div>
-                <div className="item-prices-old">
-                    ${props.new_price}
+                <div className="item-price-old">
+                    ${props.old_price}
                 </div>
             </div>
         </div>
