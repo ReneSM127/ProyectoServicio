87ff06c500b4692c04c890be67d02aa88837b936
Rene Sandoval <sandovalrenemc@gmail.com>
2025-12-11 14:07:06 -0500

    Contraseña hasheada

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -1,13 +1,17 @@
 const jwt = require("jsonwebtoken");
 const User = require("../Models/User");
+const bcrypt = require("bcrypt");
 
 exports.signup = async (req, res) => {
   const { username, email, password } = req.body;
   const check = await User.findOne({ email });
   if (check) return res.status(400).json({ success: false, errors: "Email en uso" });
 
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-  const user = new User({ name: username, email, password });
+
+  const user = new User({ name: username, email, password: hashedPassword });
   await user.save();
 
   const token = jwt.sign({ user: { id: user.id } }, process.env.JWT_SECRET);
@@ -26,8 +30,11 @@ exports.login = async (req, res) => {
   const user = await User.findOne({ email });
 
   if (!user) return res.json({ success: false, errors: "Usuario no encontrado" });
-  if (user.password !== password)
-    return res.json({ success: false, errors: "Contraseña incorrecta" });
+  
+  const validPassword = await bcrypt.compare(password, user.password);
+
+  if (!validPassword)
+      return res.json({ success: false, errors: "Contraseña incorrecta" });
 
   const token = jwt.sign({ user: { id: user.id } }, process.env.JWT_SECRET);
   res.cookie('token', token, {
